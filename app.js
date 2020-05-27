bfdcaaa586303c66856a7477cc4d47a7ce8c0dec
Gus <narongrit.n@ku.th>
2020-05-27 13:43:58 +0700

    remove mana & add number of turn in log display

--- app.js
+++ app.js
@@ -1,107 +1,82 @@
-new Vue ({
+new Vue({
     el: '#app',
     data: {
-        playerHP: 100,
-        playerMP: 50,
-        monsterHP: 100,
+        playerHealth: 100,
+        monsterHealth: 100,
         gameIsRunning: false,
         turns: []
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.gameIsRunning = true;
-            this.playerHP = 100;
-            this.playerMP = 50;
-            this.monsterHP = 100;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
             this.turns = [];
         },
-        attack: function() {
-            //for player
-            var damage = this.calculateDamage(3,10);
-            this.monsterHP -= damage;
-            this.turns.unshift({
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.push({
                 isPlayer: true,
-                text: 'Player hits Monster for' + damage
+                text: 'Player hits Monster for ' + damage
             });
             if (this.checkWin()) {
                 return;
             }
-            //for monster
-            this.monsterAttack();
+
+            this.monsterAttacks();
         },
-        specialAttack: function() {
-            var damage = this.calculateDamage(10,20);
-            if (this.isEnoughMp) {
-                this.mpUsed();
-                this.monsterHP -= damage;
-                this.turns.unshift({
-                    isPlayer: true,
-                    text: 'Player hits Monster with Crit for' + damage
-                });
-                if (this.checkWin()) {
-                    return;
-                }  
-                this.monsterAttack();
-            }    
-        },
-        heal: function() {
-            if (this.isEnoughMp) {
-                if (this.playerHP == 100){
-                    alert('Your HP is Full.');
-                }
-                this.playerHP += 20;
-                this.mpUsed();
-                if (this.playerHP > 100) {
-                    this.playerHP = 100;
-                }
-                this.turns.unshift({
-                    isPlayer: true,
-                    text: 'Player heals for 20'
-                });
-                this.monsterAttack();
+        specialAttack: function () {
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.push({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
+            if (this.checkWin()) {
+                return;
             }
+            this.monsterAttacks();
         },
-        giveUp: function() {
-            alert('You gave up,YOU LOSE!!')
+        heal: function () {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.turns.push({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
+            this.monsterAttacks();
+        },
+        giveUp: function () {
             this.gameIsRunning = false;
         },
-        isEnoughMp: function() {
-            if (playerMP < 10) {
-                alert('Your MP is not enough to cast spell.')
-                return true;
-            }
-            return false;
-        },
-        mpUsed: function() {
-            this.playerMP-=10;
-        },
-        monsterAttack: function() {
-            var damage = this.calculateDamage(5,12);
-            this.playerHP -= damage;
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
-            this.turns.unshift({
+            this.turns.push({
                 isPlayer: false,
-                text: 'Monster hits Player for' + damage
+                text: 'Monster hits Player for ' + damage
             });
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
         checkWin: function() {
-            if (this.monsterHP <= 0) {
-                if (confirm('YOU WON,you killed monster')) {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
                     this.startGame();
-                }
-                else {
+                } else {
                     this.gameIsRunning = false;
                 }
                 return true;
-            }
-            else if (this.playerHP <= 0) {
-                if (confirm('YOU LOSE,monster killed you')) {
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
                     this.startGame();
-                }
-                else {
+                } else {
                     this.gameIsRunning = false;
                 }
                 return true;
